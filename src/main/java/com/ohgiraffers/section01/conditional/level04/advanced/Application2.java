f0365ab34c19335cd291814fc4bfa1b76ba9f7e7
DESKTOP-5NMV1JN\Hyeonji Lee <cloudmato00@gmail.com>
2023-05-04 17:23:41 +0900

    level04 - Application2 문제풀이 완료

--- src/main/java/com/ohgiraffers/section01/conditional/level04/advanced/Application2.java
+++ src/main/java/com/ohgiraffers/section01/conditional/level04/advanced/Application2.java
@@ -1,5 +1,7 @@
 package com.ohgiraffers.section01.conditional.level04.advanced;
 
+import java.util.Scanner;
+
 public class Application2 {
 
 	public static void main(String[] args) {
@@ -36,7 +38,36 @@ public class Application2 {
 		 * ======================
 		 * 총 급여 : 3200000
 		 * */
-		
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("월 급여 입력 : ");
+		int salary = Integer.parseInt(sc.nextLine());
+
+		System.out.print("매출액 입력 : ");
+		int sales = Integer.parseInt(sc.nextLine());
+
+		double bonusRate = 0;
+
+		if(sales >= 50000000){
+			bonusRate = 0.05;
+		} else if (sales >= 30000000) {
+			bonusRate = 0.03;
+		} else if (sales >= 10000000) {
+			bonusRate = 0.01;
+		} else {
+			bonusRate = 0;
+		}
+
+		double pay = salary + (sales * bonusRate);
+
+		System.out.println("============================");
+		System.out.println("매출액 : " + sales);
+		System.out.println("보너스율 : " + (int)(bonusRate * 100) + "%");
+		System.out.println("월 급여 : " + salary);
+		System.out.println("보너스 금액 " + (int)(sales * bonusRate));
+		System.out.println("============================");
+		System.out.println("총 급여 : " + (int)pay);
 	}
 
 }
